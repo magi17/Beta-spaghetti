a315a75cc5fba3fd0fe8b1128443bb6536b9febb
magi17 <yen-evade-subsidy@duck.com>
2025-02-06 10:50:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get('/random', (req, res) => {
     res.json({ response: randomResponse });
 });
 
-app.get('/shoti', (req, res) => {
+app.get('/shoti2', (req, res) => {
     // Path to the JSON file
     const filePath = path.join(__dirname, 'shoti.json');
 
@@ -46,6 +46,264 @@ app.get('/shoti', (req, res) => {
             res.status(500).json({ error: 'Invalid JSON format in the file.' });
         }
     });
+});
+
+
+const data = [
+  {
+    "name": "damon",
+    "description": "💋",
+    "url": "https://tikcdn.io/ssstik/7461384205109398790"
+  },
+  {
+    
+    "name": "dàmon",
+    "description": "",
+    "url": "https://tikcdn.io/ssstik/7438207933537586450"
+  },
+  {
+    "name": "MIKHA LIM.",
+    "decscription": "Sorry for the messy hair shhdhjs pls tag dc🫶🏻 #BINI #BINIPH #binimikha",
+    "url": "https://tikcdn.io/ssstik/7455231006887529735"
+  },
+  {
+  "name": "dàmon",
+  "description": "",
+  "url": "https://tikcdn.io/ssstik/7428537043879267591"
+},
+{
+  "name": "zang",
+  "description": "",
+  "url": "https://tikcdn.io/ssstik/7441226770315054356"
+},
+{
+  "name": "sanji",
+  "description": "",
+  "url": "https://tikcdn.io/ssstik/7450356992557862151"
+},
+{
+  "name": "rhedbustamante01",
+  "description": "",
+  "url": "https://tikcdn.io/ssstik/7440044797865266448"
+},
+{
+  "name": "tojift2",
+  "description": "elevate🔥 #fyp #zyxbca #cbaxyz #tojift2",
+  "url": "https://tikcdn.io/ssstik/7451208970238168327"
+},
+{
+  "name": "IG:yujinT_T",
+  "description": "#fyp",
+  "url": "https://tikcdn.io/ssstik/7448460726731017492"
+},
+{
+  "name": "totorooor",
+  "description": "last na",
+  "url": "https://tikcdn.io/ssstik/7447098941411265800"
+},
+{
+  "name": "brea",
+  "description": "THANKU FOR 100k I love you all rahhhh \u003E0\u003C #fypagee #fyppppppppppppppppppppppp #fypシ゚ #fyp",
+  "url": "https://tikcdn.io/ssstik/7460290336695340296"
+},
+{
+  "name": "rhedbustamante01",
+  "description": "Happy new year mga bading",
+  "url": "https://tikcdn.io/ssstik/7454535513354128647"
+},
+{
+  "name": "noncakeith🇵🇭🇯🇵",
+  "description": "#noncakeith",
+  "url": "https://tikcdn.io/ssstik/7447900531995036935"
+},
+{
+  "name": "ig: awe.nica_",
+  "description": "",
+  "url": "https://tikcdn.io/ssstik/7432703839498751240"
+},
+{
+  "name": "noncakeith",
+  "description": "#noncakeith",
+  "url": "https://tikcdn.io/ssstik/7434485468542078216"
+},
+{
+  "name": "noncakeith🇵🇭🇯🇵",
+  "description": "#noncakeith #latina",
+  "url": "https://tikcdn.io/ssstik/7456534357235223816"
+},
+{
+
+"name": "J.SAZ",
+"description": "LUPAYPAY #JSAZ",
+"url": "https://tikcdn.io/ssstik/7461609351082429697"
+
+},
+{
+
+"name": "J.SAZ",
+"description": "CUTIE NG RIBBON #JSAZ",
+"url": "https://tikcdn.io/ssstik/7461582929454566672"
+
+},
+{
+
+"name": "ge",
+"description": "",
+"url": "https://tikcdn.io/ssstik/7440182775593930005"
+
+},
+{
+
+"name": "L",
+"description": "",
+"url": "https://tikcdn.io/ssstik/7449957349390847239"
+
+},
+{
+
+"name": "J.SAZ",
+"description": "Draft lang muna#JSAZ",
+"url": "https://tikcdn.io/ssstik/7461051510621867280"
+
+},
+{
+
+"name": "Ig: jyrhjane",
+"description": "The bilbil is not giving chariz HAHAHAH",
+"url": "https://tikcdn.io/ssstik/7434946602755722514"
+
+},
+{
+
+"name": "_alyssa1",
+"description": "",
+"url": "https://tikcdn.io/ssstik/7443856642464025874"
+
+},
+{
+
+"name": "Matcha Lover",
+"description": "Part 2",
+"url": "https://tikcdn.io/ssstik/7443705062771182856"
+
+},
+{
+
+"name": "Jeshii Rose",
+"description": "Draft",
+"url": "https://tikcdn.io/ssstik/7447024123319962888"
+
+},
+{
+
+"name": "jewel",
+"description": "lahat na lng binabaliw",
+"url": "https://tikcdn.io/ssstik/7438541778434706706"
+
+},
+{
+
+"name": "iizy",
+"description": "delikado mahalin",
+"url": "https://tikcdn.io/ssstik/7452684453333257480"
+
+},
+{
+
+"name": "jeanniefransdolor",
+"description": "",
+"url": "https://tikcdn.io/ssstik/7461427853050023175"
+
+},
+{
+
+"name": "J.SAZ",
+"description": "Kakagising lang hehe #JSAZ",
+"url": "https://tikcdn.io/ssstik/7458948403678973200"
+
+},
+{
+
+"name": "Jeshii Rose",
+"description": "Love chubby girls!❤️❤️❤️",
+"url": "https://tikcdn.io/ssstik/7452316220902214930"
+
+},
+{
+
+"name": ":mayang",
+"description": "ayan nanaman #foryoupage #foryou #twerk",
+"url": "https://tikcdn.io/ssstik/7449284712310279431"
+
+},
+{
+
+"name": "katytastescandy",
+"description": "",
+"url": "https://tikcdn.io/ssstik/7455695392915623176"
+
+},
+{
+
+"name": "ren",
+"description": "",
+"url": "https://tikcdn.io/ssstik/7459753596196883730"
+
+},
+{
+
+"name": "jink",
+"description": "",
+"url": "https://tikcdn.io/ssstik/7458676582039784722"
+
+},
+{
+
+"name": "เมื่อไหร่จะสอบเสร็จ",
+"description": "คนข้างนอกจะเหนมั้ย👁️👁️",
+"url": "https://tikcdn.io/ssstik/7443458462228630792"
+
+},
+{
+
+"name": "jewel",
+"description": "",
+"url": "https://tikcdn.io/ssstik/7463321706388622609"
+
+},
+{
+
+"name": "ari🍓",
+"description": "pagod sha",
+"url": "https://ssscdn.io/ssstik/7462733518624279826"
+
+},
+{
+
+"name": "Hershey",
+"description": "HAHAHAHa bored naman sya HAHaa!",
+"url": "https://tikcdn.io/ssstik/7460477799682395399"
+
+},
+{
+
+"name": "dirungzi",
+"description": "다음년에 해외여행어디가게~~~ #fyp",
+"url": "https://tikcdn.io/ssstik/7444496886318337288"
+
+},
+{
+
+"name": "เพนเพ่นเพ้นท์",
+"description": "",
+"url": "https://tikcdn.io/ssstik/7457747307736386834"
+
+}
+ ];
+ 
+app.get('/shoti', (req, res) => {
+  const randomIndex = Math.floor(Math.random() * data.length);
+  res.json(data[randomIndex]);
 });
 
 
